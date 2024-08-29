be6389e839eb2af5687ca87c2782d549e24fec13
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-08-28 21:17:24 -0700

    added entry json generation

--- src/views/Entry.vue
+++ src/views/Entry.vue
@@ -2,20 +2,60 @@
 
     import { ref } from 'vue'
 
+    import { setToStorage, getFromStorage, removeFromStorage } from '@/utils/localStorage';
+
     import NumEntry from '@/components/NumEntry.vue';
+
+    import { Button } from '@/components/ui/button';
     
     const TEST_SCHEMA = {
         'inputs': [
             {
                 'type': 'num',
-                'name': 'speaker'
+                'id': 'auto.test.speaker',
+                'label': 'Speaker',
+            },
+            {
+                'type': 'num',
+                'id': 'amp',
+                'label': 'Amp',
             },
         ]
+    }
+
+    const test = ref({})
+
+    function getEntryJson() {
+        var entry = {}
+        for (var input of TEST_SCHEMA.inputs) {
+            if (input.id.includes('.')) {
+                var split = input.id.split('.')
+                var inter = {}
+                inter[split.at(-1)] = getFromStorage(input.id)
+                split.pop()
+                while (split.length > 1) {
+                    let temp = {}
+                    Object.assign(temp, inter)
+                    inter = {}
+                    inter[split.at(-1)] = temp
+                    split.pop()
+                }
+                entry[split[0]] = inter
+            } else {
+                entry[input.id] = getFromStorage(input.id)
+            }
+        }
+        console.log(entry)
+        return entry
     }
 
 </script>
 
 
 <template>
-    
+    <ul v-for="input in TEST_SCHEMA.inputs" :key="input">
+        <NumEntry v-if="input.type === 'num'" :name="input.id" :label="input.label" />
+    </ul>
+    <Button @click.stop.prevent="test=getEntryJson()">test</Button>
+    <p>{{ test }}</p>
 </template>
